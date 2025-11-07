22776929f7aca25c74b56887f170468984378f36
Mikey Nichols <mnix@journeytocode.io>
2025-11-07 12:01:56 -0500

    adds an array of initial todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css';
 
 function App() {
+  const todos = [
+    { id: 1, title: 'review resources' },
+    { id: 2, title: 'take notes' },
+    { id: 3, title: 'code out app' },
+  ];
   return (
     <div>
       <h1>My Todos</h1>
