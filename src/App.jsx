3e911ff4f3fc2cd546bc7d19c2110f8f802e8a5a
Mikey Nichols <mnix@journeytocode.io>
2025-11-07 12:02:27 -0500

    maps over todos to render them on screen

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,11 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
