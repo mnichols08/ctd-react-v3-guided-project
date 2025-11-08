cfe56b558986ce2a61046019e2999752392103bd
Mikey Nichols <mikenichols410@gmail.com>
2025-11-08 00:38:08 -0500

    migrate TodoList component into separate file

---
+++ src/components/TodoList/TodoList.component.jsx
@@ -0,0 +1,17 @@
+import './TodoList.styles.css';
+function TodoList() {
+  const todos = [
+    { id: 1, title: 'review resources' },
+    { id: 2, title: 'take notes' },
+    { id: 3, title: 'code out app' },
+  ];
+  return (
+    <ul className="todo-list">
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
