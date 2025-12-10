3ed50eb49e60388e3b9f4f3ad41b3ca4039de505
Mikey Nichols <mikenichols410@gmail.com>
2025-12-09 23:19:22 -0500

    removes key from list items

--- src/components/TodoListItem/TodoListItem.component.jsx
+++ src/components/TodoListItem/TodoListItem.component.jsx
@@ -1,7 +1,7 @@
 import './ToDoListItem.styles.css';
 
 function TodoListItem({ todo }) {
-  return <li key={todo.id}>{todo.title}</li>;
+  return <li>{todo.title}</li>;
 }
 
 export default TodoListItem;
