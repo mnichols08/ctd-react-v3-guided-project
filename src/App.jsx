85999c594268abf86cf26d2465c881b111d83415
Mikey Nichols <mnix@journeytocode.io>
2026-01-14 18:48:01 -0500

    Utilizes `BASE_URL` instead of redeclaring it

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,10 @@ const DEFAULT_HEADERS = {
 
 const encodeUrl = ({ sortField, sortDirection, queryString }) => {
   let searchQuery = '';
-  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
   let sortQuery = `sort[0][field]=${sortField}&sort[0][direction]=${sortDirection}`;
   if (queryString)
     searchQuery = `&filterByFormula=SEARCH("${queryString}",+title)`;
-  return encodeURI(`${url}?${sortQuery}${searchQuery}`);
+  return encodeURI(`${BASE_URL}?${sortQuery}${searchQuery}`);
 };
 
 const createPayload = (id, fields) => ({
