e6538ec7861260e332abcda5627494466e8fc8d3
Mikey Nichols <mnix@journeytocode.io>
2026-02-14 15:42:21 -0500

    Update Vitest configuration to exclude  multicalc-tests

--- vitest.config.js
+++ vitest.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/setupTests.js',
+    exclude: ['multicalc-tests/**', '**/node_modules/**'],
   },
 });
